2b3f504b2f2e67cb05cd59d0f73d930a9faf03ac
Masato Onda <cmj14037@ict.nitech.ac.jp>
2022-07-20 20:22:21 +0900

    modify code(1st time)

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from skimage.io import imread
 # download the image
 img_url = 'https://commons.wikimedia.org/wiki/File:Commons-logo.svg#/media/ファイル:Commons-logo.svg'
 
-im = imread(img_url))
+im = imread(img_url)))
 
 st.image(im, caption='image from wikimedia commons',
          use_column_width=True)
